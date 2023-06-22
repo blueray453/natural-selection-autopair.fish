2c9f0448d9bedb048895a332ebd386f0dd9b8210
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2023-06-23 00:32:39 +0600

    Fix _right

--- functions/_right.fish
+++ functions/_right.fish
@@ -2,9 +2,9 @@ function _right
     set --local has_selection (commandline --selection-start)
 
     if test -n "$has_selection"
+        commandline -f forward-single-char
         commandline -f end-selection
-        commandline -f forward-char
     else
-        commandline -f forward-char
+        commandline -f forward-single-char
     end
 end
